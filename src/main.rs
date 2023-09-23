fc806fa0449cfb0e3b29749e556ffaf388f6de7a
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2023-09-23 10:38:52 +0200

    Experiment with erikfran-chess library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use chess::{Game, util::{Square, File, Rank}};
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    let square = Square { file: File::E, rank: Rank::R2 };
+
+    let moves = game.possible_moves(square, true).unwrap();
+    
+    let (board_move, vec_moves) = moves;
+    
+    // println!("{:?}", boardMove);
+    println!("vec_moves = {:?}", vec_moves);
+
+    for a in board_move.rows.squares {
+        for b in a.squares {
+            if let Some(m) = b {
+                println!("{:?}", m);
+            }
+        }
+    }
 }
